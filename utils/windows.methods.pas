da20623978945364827ea97ec315c99248f86680
Rafael Picanço <cpicanco@users.noreply.github.com>
2024-07-05 01:27:27 +0100

    docs: cleaning

--- utils/windows.methods.pas
+++ utils/windows.methods.pas
@@ -1,3 +1,12 @@
+{
+  fpc-opengaze
+  Copyright (C) 2024 Carlos Rafael Fernandes Picanço.
+
+  The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
 unit windows.methods;
 
 {$mode ObjFPC}{$H+}
